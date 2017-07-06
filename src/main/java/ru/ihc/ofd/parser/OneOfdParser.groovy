378adc723fe8cfb7b10f4e201f65d2fbce47faa1
xaos <vladimir.p.polyakov@gmail.com>
2017-07-06 15:36:42 +0500

    kkms id

--- src/main/java/ru/ihc/ofd/parser/OneOfdParser.groovy
+++ src/main/java/ru/ihc/ofd/parser/OneOfdParser.groovy
@@ -8,12 +8,13 @@ class OneOfdParser {
         def ofd = new OneOfdTransport(config.play.session)
 
         config.kkt.ranges.each { String kkt, Map range ->
+            String id = range.id
             int from = range.from
             int to = range.to
 
             for (int ticketId = from; ticketId <= to; ticketId++) {
                 try {
-                    def ticket = ofd.loadTicket(kkt, "" + ticketId)
+                    def ticket = ofd.loadTicket(id, "" + ticketId)
                     def name = ticket.ticket.items[0].commodity.name
                     def txId = (name =~ /^.*ihc(\d+)$/)[0][1]
                     def op = ticket.ticket.operationType
@@ -22,7 +23,7 @@ class OneOfdParser {
                     def sum = ticket.ticket.items[0].commodity.sum
                     def transactionId = ticket.ticket.transactionId
                     def date = Date.parse("yyyy-MM-dd'T'HH:mm:ss", ticket.ticket.insertedAt).format("yyyy-MM-dd HH:mm:ss")
-                    def url = "https://org.1-ofd.ru/#/kkms/234087/tickets/${transactionId}"
+                    def url = "https://org.1-ofd.ru/#/kkms/${kkt}/tickets/${transactionId}"
 
                     println "replace into _ofd_receipts (id, tx_id, dt, tx_type, full_sum, price, quantity, url) values (\"${transactionId}\", ${txId}, \"${date}\", ${op}, ${sum}, ${price}, ${quantity}, \"${url}\");"
                 } catch (Exception e) {
