3bbc0726f085b794dbcc71a961e1b8e9bbe10ded
xaos <vladimir.p.polyakov@gmail.com>
2017-07-11 19:04:03 +0500

    авторизация, поиск последней транзакции, разбивка по префиксам

--- src/main/java/ru/ihc/ofd/parser/OneOfdParser.groovy
+++ src/main/java/ru/ihc/ofd/parser/OneOfdParser.groovy
@@ -2,35 +2,96 @@ package ru.ihc.ofd.parser
 
 class OneOfdParser {
 
-    static void main(String[] args) {
-        ConfigObject config = new ConfigSlurper().parse(new File("config.groovy").text)
+    ConfigObject config;
+    Map<String, FileWriter> outs = [:]
+    Map<String, OneOfdTransport> sessions = [:]
 
-        def ofd = new OneOfdTransport(config.play.session)
+    OneOfdParser() {
+        config = new ConfigSlurper().parse(new File("config.groovy").text)
+    }
 
+    int countRanges() {
+        // сколько всего запросов
+        def max = 0
         config.kkt.ranges.each { String kkt, Map range ->
+            if (range.to == null) {
+                def ofd = sessions[range.account]
+                range.to = ofd.requestCount(kkt)
+                println "${kkt} has ${range.to} tickets"
+            }
+            max += range.to - range.from + 1
+        }
+        return max
+    }
+
+    void openOut() {
+        // подготовка всех файлов
+        config.prefixes.each { String prefix ->
+            outs[prefix] = new FileWriter("${prefix}.sql", false)
+        }
+    }
+
+    void auth() {
+        // авторизация во все аккаунты
+        config.credentials.each { String login, String password ->
+            sessions[login] = new OneOfdTransport(login, password)
+        }
+    }
+
+    void parse() {
+        auth()
+        openOut()
+        def max = countRanges()
+
+        def count = 0
+        def percent = 0
+        config.kkt.ranges.each { String kkt, Map range ->
+            def ofd = sessions[range.account]
+
             String id = range.id
             int from = range.from
             int to = range.to
 
             for (int ticketId = from; ticketId <= to; ticketId++) {
+                count++
                 try {
-                    def ticket = ofd.loadTicket(id, "" + ticketId)
+                    def ticket = ofd.requestTicket(id, "" + ticketId)
                     def name = ticket.ticket.items[0].commodity.name
-                    def txId = (name =~ /^.*ihc(\d+)$/)[0][1]
+                    def prefix = config.prefixes.find {
+                        name ==~ /^.*${it}(\d+)$/
+                    }
+                    if (!prefix) {
+                        throw new IllegalArgumentException("${kkt} => ${ticketId}: unrecognized name: ${name}")
+                    }
+
+                    def txId = (name =~ /^.*${prefix}(\d+)$/)[0][1]
                     def op = ticket.ticket.operationType
                     def price = ticket.ticket.items[0].commodity.price
                     def quantity = ticket.ticket.items[0].commodity.quantity
                     def sum = ticket.ticket.items[0].commodity.sum
                     def transactionId = ticket.ticket.transactionId
-                    def date = Date.parse("yyyy-MM-dd'T'HH:mm:ss", ticket.ticket.insertedAt).format("yyyy-MM-dd HH:mm:ss")
+                    def date = Date.parse("yyyy-MM-dd'T'HH:mm:ss", ticket.ticket.transactionDate).format("yyyy-MM-dd HH:mm:ss")
                     def url = "https://org.1-ofd.ru/#/kkms/${kkt}/tickets/${transactionId}"
 
-                    println "replace into _ofd_receipts (id, tx_id, dt, tx_type, full_sum, price, quantity, url) values (\"${transactionId}\", ${txId}, \"${date}\", ${op}, ${sum}, ${price}, ${quantity}, \"${url}\");"
+                    def sql = "replace into _ofd_receipts (id, tx_id, dt, tx_type, full_sum, price, quantity, url) values (\"${transactionId}\", ${txId}, \"${date}\", ${op}, ${sum}, ${price}, ${quantity}, \"${url}\");"
+                    outs[prefix].write(sql + "\n")
+
+                    // прогресс
+                    def nextPercent = Math.round(100d * count / max)
+                    if (nextPercent > percent) {
+                        percent = nextPercent
+                        println "${kkt} => ${ticketId} / ${percent}%"
+                    }
                 } catch (Exception e) {
-                    println "-- ${e}"
+                    println "${e}"
                 }
             }
         }
+
+        outs.values()*.close()
     }
 
+    static void main(String[] args) {
+        new OneOfdParser().parse()
+    }
 }
--- src/main/java/ru/ihc/ofd/parser/OneOfdTransport.groovy
+++ src/main/java/ru/ihc/ofd/parser/OneOfdTransport.groovy
@@ -11,13 +11,50 @@ class OneOfdTransport {
         this.playSession = playSession
     }
 
-    Map loadTicket(String kkt, String ticketId) {
-        String url = "${baseUrl}ticket/${kkt}_${ticketId}"
+    OneOfdTransport(String login, String password) {
+        this.playSession = this.login(login, password)
+    }
+
+    Map request(String url) {
         URL apiUrl = new URL(url)
-        URLConnection urlConn = apiUrl.openConnection();
-        urlConn.setRequestProperty("Cookie", "PLAY_SESSION=${playSession}");
+        HttpURLConnection urlConn = (HttpURLConnection) apiUrl.openConnection();
+        urlConn.setRequestProperty("Cookie", playSession);
         def result = new JsonSlurper().parse(urlConn.inputStream.newReader())
+        urlConn.disconnect()
         return result
     }
 
+    String login(String login, String password) {
+        def url = ("${baseUrl}user/login")
+        def json = "{\"login\":\"${login}\",\"password\":\"${password}\"}"
+
+        URL apiUrl = new URL(url)
+        HttpURLConnection connection = (HttpURLConnection) apiUrl.openConnection();
+
+        connection.setRequestMethod("POST");
+        connection.setRequestProperty("Content-Type", "application/json;charset=UTF-8");
+        connection.setRequestProperty("Content-Length", "" + Integer.toString(json.getBytes().length));
+        connection.setUseCaches(false);
+        connection.setDoInput(true);
+        connection.setDoOutput(true);
+
+        DataOutputStream wr = new DataOutputStream(connection.getOutputStream());
+        wr.writeBytes(json);
+        wr.flush();
+        wr.close();
+
+        def cookie = connection.getHeaderField("Set-Cookie");
+
+        connection.disconnect()
+
+        return cookie
+    }
+
+    Map requestTicket(String kkt, String ticketId) {
+        return request("${baseUrl}ticket/${kkt}_${ticketId}")
+    }
+
+    int requestCount(String kkt) {
+        return request("${baseUrl}kkms/${kkt}/transactions?page=1&pageSize=1").transactions[0].fiscalDocumentNumber
+    }
 }
